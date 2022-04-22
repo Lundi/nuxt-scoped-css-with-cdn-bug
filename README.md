136af558fec6fe61d6ddeccde8775f1474639d9a
Lundi <chris.lloyd@arm.com>
2022-04-22 11:56:38 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-d6wvrv-4cb7xz
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-d6wvrv-4cb7xz)
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Test scoped css from cdn</h1>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
